4c3bc4e6a7f7155a9d415a4401e68bb7a8af344d
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-08 21:26:31 +0300

    feat: header, layout, paths, custom-button, login, register - initialization

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Button, Layout, Space, Typography} from 'antd';
+import styles from './index.module.css';
+import {TeamOutlined} from '@ant-design/icons';
+import {CustomButton} from '../custom-button';
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
+
+
+const Header = () => {
+    return (
+        <Layout.Header className={styles.header}>
+            <Space>
+                <TeamOutlined className={styles.teamIcon}/>
+                <Link to={Paths.home}>
+                    <CustomButton type="ghost">
+                        <Typography.Title level={1}>
+                            Сотрудники
+                        </Typography.Title>
+                    </CustomButton>
+                </Link>
+            </Space>
+            <Space>
+                <Link to={Paths.register}>
+                    <CustomButton type='ghost'>Зарегистрироваться</CustomButton>
+                </Link>
+                <Link to={Paths.login}>
+                    <CustomButton type='ghost'>Войти</CustomButton>
+                </Link>
+            </Space>
+        </Layout.Header>
+    );
+};
+
+export default Header;
