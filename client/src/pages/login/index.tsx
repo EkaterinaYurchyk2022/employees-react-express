c23317155b7601b18b695e53ff5e48008e7e49e6
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-09 19:16:26 +0300

    feat: register form

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -38,7 +38,6 @@ const Login = () => {
                 </Card>
             </Row>
         </Layout>
-
     );
 };
 
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import Layout from '../../components/layout';
+import {Card, Form, Row, Space, Typography} from 'antd';
+import CustomInput from '../../components/custom-input';
+import PasswordInput from '../../components/password-input';
+import {CustomButton} from '../../components/custom-button';
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
 
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <Layout>
+            <Row align="middle" justify="center">
+                <Card title="Зарегистрируйтесь" style={{width: '30rem'}}>
+                    <Form onFinish={() => null}>
+                        <CustomInput
+                            placeholder="Имя"
+                            name="name"
+                        /><CustomInput
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                    />
+                        <PasswordInput
+                            name="password"
+                            placeholder="Пароль"
+                        />
+                        <PasswordInput
+                            name="confirmPassword"
+                            placeholder="Введите пароль еще раз"
+                        />
+                        <CustomButton
+                            type="primary"
+                            htmlType="submit"
+                        >
+                            Зарегистрироваться
+                        </CustomButton>
+                    </Form>
+                    <Space direction="vertical" size="large">
+                        <Typography.Text>
+                            Уже зарегистрированы? <Link to={Paths.login}>Войти</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
+        </Layout>
     );
 };
 
