13bebd53115f59be0919f3c1848b769127edc510
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-11 12:50:47 +0300

    feat: remove employee from the table, modal fix: employee api remove employee refactor url

--- client/src/pages/employee/index.tsx
+++ client/src/pages/employee/index.tsx
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
-import {Navigate, useNavigation, useParams} from 'react-router-dom';
-import {useEditEmployeeMutation, useGetEmployeeQuery} from '../../app/services/employees';
+import {Link, Navigate, useNavigate, useParams} from 'react-router-dom';
+import {useGetEmployeeQuery, useRemoveEmployeeMutation} from '../../app/services/employees';
 import {useSelector} from 'react-redux';
 import {selectUser} from '../../features/auth/authSlice';
 import Layout from '../../components/layout';
-import {Descriptions} from 'antd';
+import {Descriptions, Divider, Space} from 'antd';
 import DescriptionsItem from 'antd/es/descriptions/Item';
+import {CustomButton} from '../../components/custom-button';
+import {DeleteOutlined, EditOutlined} from '@ant-design/icons';
+import ErrorMessage from '../../components/error-message';
+import Modal from 'antd/es/modal/Modal';
+import {Paths} from '../../paths';
+import {isErrorWithMessage} from '../../utils/is-error-with-message';
 
 const Employee = () => {
-    const navigate = useNavigation();
+    const navigate = useNavigate();
     const params = useParams<{ id: string }>();
 
     const [error, setError] = useState('');
@@ -16,7 +22,8 @@ const Employee = () => {
 
     const user = useSelector(selectUser);
     const {data, isLoading} = useGetEmployeeQuery(params.id || '');
-    const [employee] = useEditEmployeeMutation();
+    const [removeEmployee] = useRemoveEmployeeMutation();
+
 
     if (isLoading) {
         return <span>Идет загрузка</span>
@@ -24,6 +31,28 @@ const Employee = () => {
 
     if (!data) {
         return <Navigate to="/"/>
+    }
+
+    const showModal = () => {
+        setModalOpen(true)
+    }
+    const hideModal = () => {
+        setModalOpen(false)
+    }
+
+    const handleDeleteUser = async () => {
+        hideModal()
+        try {
+            await removeEmployee(data.id).unwrap();
+            navigate(`${Paths.status}/deleted`)
+        } catch (error) {
+            const mayBeError = isErrorWithMessage(error)
+            if (mayBeError) {
+                setError(error.data.message)
+            } else {
+                setError('Неизвестная ошибка')
+            }
+        }
     }
 
     return (
@@ -39,6 +68,43 @@ const Employee = () => {
                     {data.address}
                 </Descriptions.Item>
             </Descriptions>
+            {
+                user?.id === data.userId && (
+                    <>
+                        <Divider orientation="left">Действия</Divider>
+                        <Space>
+                            <Link to={`/employee/edit/${data.id}`}>
+                                <CustomButton
+                                    shape="round"
+                                    type="default"
+                                    icon={<EditOutlined/>}
+                                >
+                                    Редактировать
+                                </CustomButton>
+                            </Link>
+                            <CustomButton
+                                shape="round"
+                                danger
+                                onClick={showModal}
+                                icon={<DeleteOutlined/>}
+                            >
+                                Удалить
+                            </CustomButton>
+                        </Space>
+                    </>
+                )
+            }
+            <ErrorMessage message={error}/>
+            <Modal
+                title="Подтвердите удаление"
+                open={modalOpen}
+                onOk={handleDeleteUser}
+                onCancel={hideModal}
+                okText="Подтвердить"
+                cancelText="Отменить"
+            >
+                Вы действительно хотите удалить сотрудника из таблицы?
+            </Modal>
         </Layout>
     );
 };
